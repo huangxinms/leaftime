eb381545f58f23638fe2152e94558d9e4958d735
HuangXin <huangxinms@gmail.com>
2013-04-06 01:34:16 +0800

    增加母板页

--- leaf/__init__.py
+++ leaf/__init__.py
@@ -1,12 +1,14 @@
 #-*- coding:utf-8 -*-
 
 from flask import Flask
-from leaf.corelib.flask_login import LoginManager
+from leaf.corelib.flask_login import LoginManager,get_user_id
 
 
 #-- create app --
 app = Flask(__name__)
 app.config.from_object('leaf.config')
+
+app.jinja_env.globals.update(get_user_id=get_user_id)
 
 lm = LoginManager()
 lm.init_app(app)
