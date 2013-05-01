f1ee6389c581d0c207aa183c0b359f7e6d8acdd2
HuangXin <huangxinms@gmail.com>
2013-05-02 01:45:54 +0800

    将static改为绝对路径

--- leaf/__init__.py
+++ leaf/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object('leaf.config')
 
 app.jinja_env.globals.update(get_user_id=get_user_id)
-app.jinja_env.globals.update(static='static')
+app.jinja_env.globals.update(static='/static')
 
 lm = LoginManager()
 lm.init_app(app)
